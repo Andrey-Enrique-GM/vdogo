0946dd750af796ad6243e59f57219e9ec8921ea0
Alumno CITEV <alumnog@itson.edu.mx>
2026-03-06 09:40:18 -0700

    segundo

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# underscope methods and properties
+
+if __name__ == '__main__':
+    app.run()
