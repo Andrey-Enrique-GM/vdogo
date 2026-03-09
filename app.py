d0223da347a18b0c6ceff532ea62fc94124e5390
Alumno CITEV <alumnog@itson.edu.mx>
2026-03-09 09:56:05 -0700

    tercer commit, se agrego la ocion para registrarse

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')
