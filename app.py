311feac13a766376f4242d307473a0098c9892af
Andrey <andreyenrique132@gmail.com>
2026-03-18 16:49:53 -0700

    sexto, se trato de hacer un login, falta arreglarlo y ademas implementar sweetalert2 para que se vea mas bonito

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 from flask import Flask, render_template, request, jsonify
 from entities.user import User
+import pymysql
 
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
+
+
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')
+
+
 
 @app.route('/api/users', methods=['POST'])
 def create_user():
@@ -23,9 +32,37 @@ def create_user():
     email = data.get('email')
     password = data.get('password')
 
-    User.save(name, email, password)
+    if User.check_email_exists(email):
+        return jsonify({"success": False, "message": "El correo electrónico ingresado ya se encuentra registrado."}), 409
 
-    return jsonify({"success": True})
+    if User.save(name, email, password):
+        return jsonify({"success": True, "message": "Su cuenta fue creada correctamente."}), 201
+    else:
+        return jsonify({"success": False, "message": "Ocurrió un error al crear su cuenta. Intente de nuevo"}), 500
+
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    email = data.get('email')
+    password = data.get('password')
+
+    user = User.check_login(email, password)
+
+    if user:
+        return jsonify({
+            "success": True, 
+            "message": "Inicio iniciada correctamente."
+        }), 200
+    else:
+        return jsonify({
+            "success": False, 
+            "message": "Los datos de acceso ingresados no son correctos. Intente de nuevo."
+        }), 401
+
+
 
 if __name__ == '__main__':
     app.run()
